d383a366755a51c286e9aef47fbf1a2e1d831570
MortalFlame21 <markgio2100@gmail.com>
2026-04-04 13:34:24 +1100

    update: main.cpp. generate n allocs and k deallocs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
+#include <algorithm>
 #include <iostream>
+#include <random>
+#include <ranges>
+#include <vector>
 
 #include "bb/alloc.h"
 
-int main() {
-    auto* ptr{bb::alloc<int>()};
-    *ptr = 4;
-    std::cout << *ptr << '\n';
-    bb::dealloc(ptr);
+std::random_device rd{};
+std::mt19937 mt{rd()};
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: mem_allocations <NUM_ALLOCATIONS> <NUM_DEALLOCATIONS>";
+        return EXIT_FAILURE;
+    }
+
+    // invariant: allocs >= deallocs.
+    int num_deallocs{std::max(1, std::stoi(argv[2]))};
+    int num_allocs{std::max({1, std::stoi(argv[1]), num_deallocs})};
+    std::vector<int*> g_allocations(num_allocs, {});
+
+    // we don't know the amount of
+    try {
+        namespace rng = std::ranges;
+
+        rng::generate(g_allocations, [] { return bb::alloc<int>(); });
+        rng::for_each_n(g_allocations.begin(), num_allocs,
+                        [&](auto ptr) { *ptr = mt(); });
+
+        rng::shuffle(g_allocations, mt);
+
+        rng::for_each_n(g_allocations.begin(), num_deallocs,
+                        [](auto ptr) { bb::dealloc(ptr); });
+    } catch (std::exception& e) {
+        std::cerr << "caught: " << e.what() << '\n';
+        std::cerr << "\tpossibly too much allocations (" << num_allocs << ")\n";
+    }
 }
