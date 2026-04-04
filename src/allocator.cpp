2b6c78bfa1c9b1e873c8d03b51cad5ff7246bb33
MortalFlame21 <markgio2100@gmail.com>
2026-04-04 22:14:47 +1100

    fix: allocator.cpp. fix class compilation error. i.e. move templates func.

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -1,23 +1,19 @@
-#include <allocator.h>
+#include <algorithm>
+#include <list>
+#include <new>
+#include <ranges>
 
-class allocator::bad_dealloc : std::exception {
-public:
-    bad_dealloc() {}
-    const char* what() const noexcept override {
-        return "bb::bad_dealloc: invalid pointer or double free";
-    }
-};
+#include "allocator.h"
 
-allocator::allocator() : strategy_{strategy::FIRST_FIT} {}
+allocator::bad_dealloc::bad_dealloc() {}
 
-template <typename T>
-T* allocator::alloc() {
-    size_t sz{sizeof(T)};
-    if (T * blk{req_blk<T>(sz)})
-        return blk;
-    else
-        return req_space<T>(sz);
+const char* allocator::bad_dealloc::what() const noexcept {
+    return "bb::bad_dealloc: invalid pointer or double free";
 }
+
+allocator::allocator() : allocator(strategy::FIRST_FIT) {}
+
+allocator::allocator(strategy s) : strategy_{s} {}
 
 void allocator::dealloc(void* ptr) {
     if (!ptr)
@@ -47,59 +43,4 @@ const char* allocator::to_string(strategy s) {
     default:
         return "unknown";
     }
-}
-// allocation strategies
-template <typename T>
-T* allocator::first_fit(size_t sz) {
-    // a first fit scan
-    auto it{std::ranges::find_if(blk_free_, [sz](auto& a) { return a->size_ < sz; })};
-
-    if (it == blk_free_.end())
-        return nullptr;
-
-    (*it)->flag_ = bb::flag::USED; // validate magic again
-    blk_used_.push_back(*it);      // move from free to used
-    blk_free_.erase(it);
-
-    return static_cast<T*>((*it)->addr_);
-}
-
-template <typename T>
-T* allocator::best_fit(size_t sz) {
-    return nullptr;
-}
-
-template <typename T>
-T* allocator::worst_fit(size_t sz) {
-    return nullptr;
-}
-
-template <typename T>
-T* allocator::req_blk(size_t sz) {
-    switch (strategy_) {
-    case strategy::FIRST_FIT:
-        return first_fit<T>(sz);
-    case strategy::BEST_FIT:
-        return best_fit<T>(sz);
-    case strategy::WORST_FIT:
-        return worst_fit<T>(sz);
-    default:
-        return nullptr;
-    }
-}
-
-template <typename T>
-T* allocator::req_space(size_t sz) {
-    // confirm our request to increment the heap, sz + header
-    auto req{sbrk(static_cast<intptr_t>(sz + sizeof(alloc_t)))};
-    if (req == (void*)-1) // could not find
-        throw std::bad_alloc();
-
-    auto* blk{static_cast<alloc_t*>(req)};
-    blk->addr_ = static_cast<char*>(req) + sizeof(alloc_t); // skip header
-    blk->size_ = sz;
-    blk->flag_ = flag::USED;
-    blk_free_.push_back(blk);
-
-    return static_cast<T*>(blk->addr_);
 }
