9006536952ebdf29e7de1d062ec3a8806581e5be
MortalFlame21 <markgio2100@gmail.com>
2026-04-04 14:09:45 +1100

    feat: main.cpp. allocs to use such various strategies.

--- src/main.cpp
+++ src/main.cpp
@@ -20,18 +20,30 @@ int main(int argc, char* argv[]) {
     int num_allocs{std::max({1, std::stoi(argv[1]), num_deallocs})};
     std::vector<int*> g_allocations(num_allocs, {});
 
-    // we don't know the amount of
+    // we don't know the amount of num_allocs, use try catch incase of std::bad_alloc or
+    // bb::bad_dealloc
     try {
         namespace rng = std::ranges;
+        using stgy = bb::alloc_strategy;
 
-        rng::generate(g_allocations, [] { return bb::alloc<int>(); });
-        rng::for_each_n(g_allocations.begin(), num_allocs,
-                        [&](auto ptr) { *ptr = mt(); });
+        for (int s{}; s < static_cast<int>(stgy::MAX); ++s) {
+            auto strategy{static_cast<stgy>(s)};
 
-        rng::shuffle(g_allocations, mt);
+            // time start
 
-        rng::for_each_n(g_allocations.begin(), num_deallocs,
-                        [](auto ptr) { bb::dealloc(ptr); });
+            for (auto& ptr : g_allocations) {
+                ptr = bb::alloc<int>(strategy);
+                *ptr = mt();
+            }
+
+            rng::shuffle(g_allocations, mt);
+
+            for (int i{}; i < num_deallocs; ++i) {
+                bb::dealloc(g_allocations[i]);
+            }
+
+            // time end
+        }
     } catch (std::exception& e) {
         std::cerr << "caught: " << e.what() << '\n';
         std::cerr << "\tpossibly too much allocations (" << num_allocs << ")\n";
